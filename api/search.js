fe7ad672a351155cdc907fbe6d43aee859ed1812
davidnguyenc1 <davidnguyenc1>
2025-12-16 00:10:31 -0800

    testing

--- api/search.js
+++ api/search.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
       res.status(200).json(data.businesses);
     } catch {
       res.status(500).json({ error: "Failed to fetch Yelp data" });
-    }
+    } 
   }
   
